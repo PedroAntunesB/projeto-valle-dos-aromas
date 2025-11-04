50cf2fde3ac423b2633ee43d8dc1f4ecb3edf9ba
PedroAntunes <pedrohantunnes@gmail.com>
2025-11-03 21:27:01 -0300

    first commit

--- backend/routes/app-routes.js
+++ backend/routes/app-routes.js
@@ -2,7 +2,7 @@ import express from "express";
 import path, { resolve, dirname } from "path";
 import { fileURLToPath } from "url";
 const app = express();
-const port = 5000;
+const port = 8080;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -11,6 +11,6 @@ app.use(express.static("frontend"));
 app.get("/", (req, res) => {
     res.sendFile(resolve(__dirname, "frontend", "index.html"));
 });
-app.listen(port, () => console.log(`Servidor rodando em localhost:${port}`));
+app.listen(port, () => console.log(`Servidor rodando`));
 
 export default app;
